3f6998e4495688c0912425dc0cc03b71f40e5d53
Augusto Gonzalez Bonorino <Augusto.GonzalezBonorino1@marist.edu>
2021-10-08 19:17:56 -0400

    Update mainGonzalezBonorino.java
    Added quick sort plus cleaned main so the shuffling happens inside the methods once the sorting algo is called instead of shuffling every time before calling the sort method

--- assignment2/Assignment2/src/mainGonzalezBonorino.java
+++ assignment2/Assignment2/src/mainGonzalezBonorino.java
@@ -21,6 +21,7 @@ import java.io.*;
 public class mainGonzalezBonorino {
 	
 	static Scanner keyboard = new Scanner(System.in);
+	public static int quickSortComparisons = 0;
 	
 	public static void main(String[] args) {
 		
@@ -97,25 +98,43 @@ public class mainGonzalezBonorino {
 			
 		} // catch no such element
 		
+		// shuffle myMagicList
+		//knuthShuffle(myMagicList);
+		
 		// Selection sort
 		
 		System.out.println(selectionSort(myMagicList));
 
 		
 		// shuffle myMagicList
-		knuthShuffle(myMagicList);
+		//knuthShuffle(myMagicList);
 
 		
 		// Insertion sort
 
 		System.out.println(insertionSort(myMagicList));
 		
-		for(int i = 0; i < myMagicList.length; i++)
-			System.out.println(myMagicList[i]);
+		
+		// shuffle myMagicList
+		//knuthShuffle(myMagicList);
+		
+		// Merge Sort
+		
+		System.out.println(mergeSort(myMagicList, 0));
+		
+		// shuffle myMagicList
+		//knuthShuffle(myMagicList);
+		
+		// Quick Sort
+		
+		System.out.println(quickSort(myMagicList, 0, myMagicList.length - 1));
+		
+
 	} // main
 	
 	public static int selectionSort(String[] magicList) {
 		
+		knuthShuffle(magicList);
 		int len = magicList.length;
 		int numComparisons = 0;
 		
@@ -147,6 +166,7 @@ public class mainGonzalezBonorino {
 	
 	public static int insertionSort(String[] magicList) {
 		
+		knuthShuffle(magicList);
 		int len = magicList.length;
 		int numComparisons = 0;
 		
@@ -188,6 +208,136 @@ public class mainGonzalezBonorino {
 
 		
 	} // Knuth shuffle
+	
+	public static int mergeSort(String[] magicList, int comparisons) {
+
+		
+		int len = magicList.length;
+
+		if (len < 2)
+			return comparisons;
+		
+		// Array's midpoint
+		int midPoint = len / 2;
+		
+		// Left and right sub-arrays
+		String[] left = new String[midPoint];
+		String[] right = new String[len - midPoint];
+		
+		// Populate sub-arrays
+		for (int i = 0; i < midPoint; i++)
+			left[i] = magicList[i];
+		
+		for (int j = midPoint; j < len; j++)
+			right[j - midPoint] = magicList[j];
+		
+		// Recursive call to keep dividing sub-arrays
+		comparisons = mergeSort(left, comparisons);
+		comparisons = mergeSort(right, comparisons);
+		comparisons = merge(left, right, magicList, comparisons);
+		
+		return comparisons;
+		
+	}
+	
+	public static int merge(String[] left, String[] right, String[] magicList, int comparisons) {
+		
+		// merge sub-arrays back together
+		int i = 0;
+		int j = 0;
+		int k = 0;
+ 		
+		knuthShuffle(magicList);
+		while (i < left.length && j < right.length) {
+			
+			comparisons++;
+			
+			if (left[i].compareToIgnoreCase(right[j]) < 0 ) {
+				
+				magicList[k] = left[i];
+				i++;
+				
+			} // if statement
+			
+			else {
+				
+				magicList[k] = right[j];
+				j++;
+				
+			} // else statement
+			
+			k++; 
+			
+		} // while loop
+		
+		
+		while (i < left.length) {
+			
+			magicList[k] = left[i];
+			i++;
+			k++;
+			
+		} // while loop
+		
+		while (j < right.length) {
+			
+			magicList[k] = right[j];
+			j++;
+			k++;
+			
+		} // while loop
+		
+		return comparisons;
+		
+	} // mergeSort
+	
+
+	public static int quickSort(String[] magicList, int lowIndex, int highIndex) {
+		
+		knuthShuffle(magicList);
+		if (lowIndex < highIndex) {
+			
+			int partIndex = partition(magicList, lowIndex, highIndex);
+			
+			quickSort(magicList, lowIndex, partIndex - 1);
+			quickSort(magicList, partIndex + 1, highIndex);
+			
+		} // if statement
+
+		return quickSortComparisons;
+	} // Quick Sort
+	
+	
+	public static int partition(String[] magicList, int lowIndex, int highIndex) {
+		
+		
+		// Take the last element as the pivot value
+		String pivot = magicList[highIndex];
+		int idx = lowIndex - 1;
+		
+		for (int j = lowIndex; j < highIndex; j++) {
+			
+			if (magicList[j].compareToIgnoreCase(pivot) < 0) {
+				
+				idx++;
+				
+				String temp = magicList[idx];
+				magicList[idx] = magicList[j];
+				magicList[j] = temp;
+				
+			} // if statement
+			
+			quickSortComparisons++;
+				
+		} // for loop
+		
+		String temp2 = magicList[idx + 1];
+		magicList[idx + 1] = magicList[highIndex];
+		magicList[highIndex] = temp2;
+		
+		return idx + 1;
+		
+	} // partition
 	
 
 } // MainGonzalezBonorino
